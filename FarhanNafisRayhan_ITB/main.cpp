f56c1a043d0b1c85ea9717b1a7037604bdd6f2cf
Farhannr28 <farhannafis281004@gmail.com>
2022-11-18 22:27:59 +0700

    First submission by Farhan Nafis Rayhan

---
+++ FarhanNafisRayhan_ITB/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Nama : Farhan Nafis Rayhan" << endl;
+    cout << "Jurusan : STEI-K" << endl;
+    cout << "Universitas : ITB" << endl;
+    cout << "Alasan ingin mengikuti GDSC : Mau memperdalam ilmu coding" << endl;
+}
